3ae508432c52404c8feeb7e33fab8e04d6484943
Brandon Keepers <brandon@opensoul.org>
2024-12-24 11:17:50 -0500

    Replace webpack with vite

--- upload.sh
+++ upload.sh
@@ -4,8 +4,7 @@ BUCKET=gs://vexflow.com
 URL=${BUCKET}/vexchords
 
 echo Building VexChords...
-npm run build
+npm run build:demo
 
 echo Uploading VexChords...
-gsutil -h "Cache-control:public,max-age=600" -m cp -a public-read -z js,map,css,html dist/* ${URL}
-gsutil -h "Cache-control:public,max-age=600" -m cp -a public-read -z html dist/demo.html ${URL}/index.html
+gsutil -h "Cache-control:public,max-age=600" -m cp -a public-read -z js,map,css,html demo/* ${URL}
