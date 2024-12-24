7fe34203ccabf1c14784f89c84b2c5a3bf3368ed
Brandon Keepers <brandon@opensoul.org>
2024-12-24 15:22:56 -0500

    Add "prepare" script to run `vite build`

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ BUCKET=gs://vexflow.com
 URL=${BUCKET}/vexchords
 
 echo Building VexChords...
-npm run build:demo
+npm run build
 
 echo Uploading VexChords...
 gsutil -h "Cache-control:public,max-age=600" -m cp -a public-read -z js,map,css,html demo/* ${URL}
